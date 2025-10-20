e47ef983ffaba47380499963252a5e2d3ed49424
extra785 <extra78578@gmail.com>
2025-10-20 18:21:24 +0300

    unknown

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { uploadHandler } from "../middlewares/uploadMiddleware.js";
 import { uploadFiles } from "../controllers/uploadController.js";
-
+import authenticate from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
-router.post("/", uploadHandler, uploadFiles);
+router.post("/",authenticate, uploadHandler, uploadFiles);
 
 export default router;
